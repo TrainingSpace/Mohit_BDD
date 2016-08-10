1a402277aa33b27fa4aca33c12049e7f96647a10
COELF003 <fernanda.menks@accenture.com>
2016-08-10 15:28:00 -0400

    NEW initial commit

---
+++ src/main/java/com/accenture/cucumber/Mohit_BDD/App.java
@@ -0,0 +1,13 @@
+package com.accenture.cucumber.Mohit_BDD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
